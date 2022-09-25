a01ce73b72d43a2eb7a0fa9f4d22cfebc4a66541
jalliaud <juampialliaud@gmail.com>
2022-09-24 19:05:44 -0600

    test 1

--- Program.cs
+++ Program.cs
@@ -1,7 +1,13 @@
-﻿Console.Write("what is your name?: ");
-string FirstName = Console.ReadLine();
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.Write("what is your name?: ");
+        string FirstName = Console.ReadLine();
 
-Console.Write("what is your last name?: ");
-string LastName = Console.ReadLine();
+        Console.Write("what is your last name?: ");
+        string LastName = Console.ReadLine();
 
-Console.WriteLine($"My name is {LastName}, {FirstName} {LastName}");
+        Console.WriteLine($"My name is {LastName}, {FirstName} {LastName}");
+    }
+}
