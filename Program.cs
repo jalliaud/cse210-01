31aaa5600eaf6710fedc29fd5835141a6ea4d2e3
jalliaud <juampialliaud@gmail.com>
2022-09-24 20:25:20 -0600

    second practice

--- Program.cs
+++ Program.cs
@@ -1,13 +1,47 @@
-﻿internal class Program
+﻿using System;
+
+namespace program_2
 {
-    private static void Main(string[] args)
+    class Program
     {
-        Console.Write("what is your name?: ");
-        string FirstName = Console.ReadLine();
+        static void Main(string[] args)
+        {
+            Console.Write("what is your Grade?: ");
+            string answer = Console.ReadLine();
+            int percent = int.Parse(answer);
 
-        Console.Write("what is your last name?: ");
-        string LastName = Console.ReadLine();
+            string grade = "";
 
-        Console.WriteLine($"My name is {LastName}, {FirstName} {LastName}");
+            if (percent >= 90)
+            {
+                grade = "A";
+            }
+            else if (percent >= 80)
+            {
+                grade = "B";
+            }
+            else if (percent >= 70)
+            {
+                grade = "C";
+            }
+            else if (percent >= 60)
+            {
+                grade = "D";
+            }
+            else
+            {
+                grade = "F";
+            }
+            Console.WriteLine($"Your grade is {grade}");
+            
+            if (percent < 70)
+            {
+                Console.WriteLine("You failed");
+            }
+            else
+            {
+                Console.WriteLine("You passed");
+            }
+        }
     }
 }
