ecf88d033782077f568d16663f1bb898588565a9
jalliaud <juampialliaud@gmail.com>
2022-09-24 21:32:46 -0600

    random number game

--- Program.cs
+++ Program.cs
@@ -1,47 +1,24 @@
-﻿using System;
+﻿
+Random randomGenerator = new Random();
+int number = randomGenerator.Next(0, 100);
 
-namespace program_2
+int guess = -1;
+while (guess != number)
 {
-    class Program
+    Console.Write("What is the magic number?(between 0-100): ");
+    guess = int.Parse(Console.ReadLine()); 
+    if (guess > number)
     {
-        static void Main(string[] args)
-        {
-            Console.Write("what is your Grade?: ");
-            string answer = Console.ReadLine();
-            int percent = int.Parse(answer);
-
-            string grade = "";
-
-            if (percent >= 90)
-            {
-                grade = "A";
-            }
-            else if (percent >= 80)
-            {
-                grade = "B";
-            }
-            else if (percent >= 70)
-            {
-                grade = "C";
-            }
-            else if (percent >= 60)
-            {
-                grade = "D";
-            }
-            else
-            {
-                grade = "F";
-            }
-            Console.WriteLine($"Your grade is {grade}");
-            
-            if (percent < 70)
-            {
-                Console.WriteLine("You failed");
-            }
-            else
-            {
-                Console.WriteLine("You passed");
-            }
-        }
+        Console.WriteLine("lower");
+    }
+    else if (guess < number)
+    {
+        Console.WriteLine("higher");
+    }
+    else 
+    {
+        Console.WriteLine("You guessed it!");
+        Console.WriteLine($"The right number was {number}");
     }
 }
+
