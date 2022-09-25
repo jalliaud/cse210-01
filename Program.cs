dced7dbbeaeb189d8cedd296ac903c7082d57b81
jalliaud <juampialliaud@gmail.com>
2022-09-24 19:03:06 -0600

    first program

--- Program.cs
+++ Program.cs
@@ -1,2 +1,7 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.Write("what is your name?: ");
+string FirstName = Console.ReadLine();
+
+Console.Write("what is your last name?: ");
+string LastName = Console.ReadLine();
+
+Console.WriteLine($"My name is {LastName}, {FirstName} {LastName}");
