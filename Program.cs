a2bdf23b0d04b7fce512c15b515666a7d60cce23
jalliaud <juampialliaud@gmail.com>
2022-09-26 19:50:02 -0600

    tictactoe

--- Program.cs
+++ Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+//Cse-210--Juan Pablo Alliaud(copyrightWarning--May_contain_some_code_inspired_by_the_example)
 namespace cse210_tictactoe
 {
     class Program
@@ -6,22 +8,25 @@ namespace cse210_tictactoe
         static void Main(string[] args)
         {
             List<string> board = NewBoard();
+            string PlayNow = "X";
+            do
+            {
+                ShowBoard(board);
+                int choice = GetMove(PlayNow);
+                MakeMove(board, choice, PlayNow);
+
+                PlayNow = NextPlayer(PlayNow);
+            }while (!GameOver(board));
+
             ShowBoard(board);
-
-
-
-
-
-
-        
-
-
+            Console.WriteLine("Good game. Thanks for playing!");
         }
+
         static List<string> NewBoard()
         {
             List<string> board = new List<string>();
 
-            for (int i = 1; i < 10; i = i+1)
+            for (int i = 1; i <= 9; i++)
             {
                 board.Add(i.ToString());
             }
@@ -31,8 +36,7 @@ namespace cse210_tictactoe
         static void ShowBoard(List<string> board)
         {
             Console.ForegroundColor = ConsoleColor.Magenta;
-            // This could be done more elegantly using loops and if statements
-            // especially if something besides 3x3 was ever anticipated.
+
             Console.WriteLine($"  {board[0]} |  {board[1]}  | {board[2]}");
             Console.WriteLine("----+-----+----");
             Console.WriteLine($"  {board[3]} |  {board[4]}  | {board[5]}");
@@ -41,5 +45,80 @@ namespace cse210_tictactoe
             Console.ForegroundColor = ConsoleColor.White;
         }
 
+        static bool GameOver(List<string> board)
+        {
+            bool GameOver = false;
+
+            if (IsWinner(board, "X") || IsWinner(board, "O") || IsTie(board))
+            {
+                GameOver = true;
+            }
+
+            return GameOver;
+        }
+        static bool IsWinner(List<string> board, string player)
+        {
+
+
+            bool isWinner = false;
+
+            if ((board[0] == player && board[1] == player && board[2] == player)
+                || (board[3] == player && board[4] == player && board[5] == player)
+                || (board[6] == player && board[7] == player && board[8] == player)
+                || (board[0] == player && board[3] == player && board[6] == player)
+                || (board[1] == player && board[4] == player && board[7] == player)
+                || (board[2] == player && board[5] == player && board[8] == player)
+                || (board[0] == player && board[4] == player && board[8] == player)
+                || (board[2] == player && board[4] == player && board[6] == player)
+                )
+            {
+                isWinner = true;
+            }
+
+            return isWinner; 
+        }
+
+        static bool IsTie(List<string> board)
+        {
+            bool foundDigit = false;
+
+            foreach (string value in board)
+            {
+                if (char.IsDigit(value[0]))
+                {
+                    foundDigit = true;
+                    break;
+                }
+            }
+
+            return !foundDigit;
+        }
+
+        static string NextPlayer(string PlayNow)
+        {
+            string nextPlayer = "X";
+
+            if (PlayNow == "X")
+            {
+                nextPlayer = "O";
+            }
+
+            return nextPlayer;
+        }
+
+        static int GetMove(string PlayNow)
+        {
+            Console.Write($"{PlayNow}'s turn to choose a square (1-9): ");
+            string Move = Console.ReadLine();
+
+            int choice = int.Parse(Move);
+            return choice;
+        }
+        static void MakeMove(List<string> board, int choice, string PlayNow)
+        {
+            int index = choice - 1;
+
+            board[index] = PlayNow;
+        }
     }
 }
